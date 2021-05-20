22f008653894579a003ed2233969e2682cebccaa
mihailtudos <mihailtudos@outlook.com>
2021-05-20 18:25:30 +0100

    added home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import products from './products';
+
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <HomeScreen products={products}/>
+          </Container>
+        </main>
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap';
+import Product from '../../components/Product/Product';
+
+const HomeScreen = (props) => {
+  const { products } = props;
+  return (
+    <React.Fragment>
+      <h1>Latest Products</h1>
+      <Row>
+        {
+          products.map((product) => 
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={product}/>
+            </Col>
+          )
+        }
+      </Row>
+    </React.Fragment>
+  )
+}
+
+export default HomeScreen
