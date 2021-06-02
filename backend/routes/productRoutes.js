7b7f8032f87d1e8b164c26deca585f924cd03a74
mihailtudos <mihailtudos@outlook.com>
2021-06-02 22:12:04 +0100

    added create and update product backend paths

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,19 @@
 import express from 'express';
 const router = express.Router();
-import {getProducts, getProductById, deleteProduct} from '../controller/productController.js';
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  createProduct
+} from '../controller/productController.js';
 import {isAdmin, protect} from "../middleware/authMiddleware.js";
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
 
-router.route("/:id").get(getProductById).delete(protect, isAdmin, deleteProduct);
+router.route("/:id")
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct);
 
 export default router;
