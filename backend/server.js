7321a43dcdc4e1212270d351abe38b9d194ed070
mihailtudos <mihailtudos@outlook.com>
2021-06-24 14:30:41 +0100

    changed the domain

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'origin');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     console.log('added')
